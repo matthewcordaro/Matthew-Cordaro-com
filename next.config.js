e1808383f773cbd787051f4f3f31cff49f14f05b
Matthew Cordaro <cordaro.matthew@gmail.com>
2025-03-10 17:37:53 -0400

    BugFix: Add image unoptimization for production builds
    Updated the Next.js configuration to include `images.unoptimized: true` for production builds. This ensures that image optimization is skipped during export, improving build performance in specific deployment scenarios.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 
 /** @type {import('next').NextConfig} */
-let nextConfig;
+let nextConfig = {};
 
 if (process.env.NODE_ENV === 'production') {
     nextConfig = {
-        output: 'export'
+        output: 'export',
+        images: {
+            unoptimized: true
+        }
     }
 }
 
